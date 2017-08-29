3f09b1f437815456562cab34d96a50f5292d504a
asherikov <asherikov@yandex.com>
2017-08-29 18:14:03 +0300

    Updated humoto (reworked configuration handling)

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,6 +1,6 @@
 /**
     @file
-    @author Jan Michalczyk 
+    @author Jan Michalczyk
     @copyright 2014-2017 INRIA. Licensed under the Apache License, Version 2.0.
     (see @ref LICENSE or http://www.apache.org/licenses/LICENSE-2.0)
 
